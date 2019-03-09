d3e87334ccbd47cb817579b45631fafa615da26d
msrodriguez3425 <msrodriguez3425@gmail.com>
2019-03-09 00:58:04 -0600

    5th push

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,8 +1,9 @@
 #Import csv module
 import csv
 
-#Assign file path
+#Assign file paths
 csvpath = "election_data.csv"
+outputPath = "electionResults.txt"
 
 with open(csvpath,newline="") as cvsfile:
     csvreader = csv.reader(cvsfile,delimiter = ',')
@@ -46,4 +47,29 @@ with open(csvpath,newline="") as cvsfile:
     print(f"Winner: {candidates[greatestIndex]}")
 
     print("--------------------------------")
+
+     # Open text file and write results
+    with open(outputPath, 'w') as text:
+        #Here I zipped the candidates and their percentages together and converted the reult into a list
+        #This allows me to print the candidates and their percentages in one go using a loop
+        candidateZip = list(zip(candidates,percentages))
+        text.write("Election Results\n\n")
+        text.write("--------------------------------\n")
+
+        text.write("Total Votes: %s\n"%totalVotes)
+
+        text.write("--------------------------------\n")
+        
+        #loop for printing candidates and their percentages
+        for candidate in candidateZip:
+            percentage = round((votes.count(candidate[0])/totalVotes)*100,3)
+            count =  votes.count(candidate[0])
+            text.write("%s: "%candidate[0])
+            text.write("%s %%"%percentage)
+            text.write(" (%s)\n"%count)
+        
+        text.write("--------------------------------\n")
+
+        text.write("Winner: %s"%candidates[greatestIndex])
+
 
--- PyPoll/test.py
+++
@@ -1,8 +0,0 @@
-import csv
-
-
-myList = [[]]*4
-
-myList[0][0].append(1)
-myList[1][1].append(2)
-print(myList)
